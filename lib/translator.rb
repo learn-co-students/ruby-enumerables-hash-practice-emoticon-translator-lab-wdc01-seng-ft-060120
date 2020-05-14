f424c5b4d91555e56c5bb2f83e22f60e402e7b39
Catrina Friday <cat.friday@gmail.com>
2020-05-14 13:16:33 -0400

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,23 @@
+require 'pry'
+require "yaml"
 # require modules here
 
-def load_library
+def load_library (file_path)
+ 
+  emoticons = YAML.load_file(file_path)
+  
+  new_hash = {
+   }
+emoticons.each  do |key, value|
+   new_hash[key] = {}
+   new_hash[key][:english] = value[0]
+   new_hash[key][:japanese] = value[1]
+
+  #binding.pry
+
+end 
+new_hash
+#iterate over emoticons, parse out the key and value of emoticons to create a new hash 
   # code goes here
 end
 
@@ -11,3 +28,4 @@ end
 def get_english_meaning
   # code goes here
 end
+
