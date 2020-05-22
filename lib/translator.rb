725f0ebc5582f4673b0e05b83b90f00524b167ff
Michael Causey <michaelhcausey@gmail.com>
2020-05-22 04:09:36 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,41 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
+def load_library(library)
   # code goes here
+  emoticons_library = YAML.load_file(library)
+  emoticons_library.each_with_object({}) do |(emoticon_name, emoticons), final_hash|
+    emoticons.each do |english, japanese|
+      final_hash[emoticon_name] = {:english => emoticons[0], :japanese => emoticons[1]}
+    end
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
 
-def get_english_meaning
+def get_english_meaning(library, japanese_emoticon)
   # code goes here
+  # input = "japanese_emoticon", output = emoticon_name
+  library_hash = load_library(library)
+  library_hash.each do |emoticon_name, emoticon_language|
+    if emoticon_language == [:japanese]
+      # need to return name of emoticon (emoticon_name?)
+      return japanese_emoticon = library_hash[:emoticon_name]
+    end
+  end
+end
+  
+  
+def get_japanese_emoticon(library, english_emoticon)
+  # code goes here
+  # input "english_emoticon", output = "japanese_emoticon"
+  # emoticon_language = 2 hashes: 1 for english, 1 for japanese 
+  library_hash = load_library(library)
+  library_hash.each do |emoticon_name, emoticon_language|
+    if emoticon_language == [:english]
+      library_hash[emoticon_name][emoticon_language][:japanese]
+    else
+      return "Sorry, that emoticon was not found"
+    end
+  end
 end
